7a069b9edaccb616e3e540cc0b90b9d86e1b0a89
Paul Lee <paul@valuepotion.com>
2018-12-04 10:41:00 +0900

    Export `Config`

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import ErrorType from "./error_type";
 import ResponseValidationContext from "./response_validation_context";
 import Response from "./response";
 import { mergeConfigs } from "./config/merge";
+import Config from "./config";
 
 export {
+  Config,
   Response,
   ResponseValidationContext as Context,
   ErrorType,
